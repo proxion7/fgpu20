7659bafb1fe971df3e7ce2d3d182f65c66c9cac7
proxion7 <proxion7@gmail.com>
2022-09-30 09:06:52 +0000

    ===Add uvm_config.h===
    This file is made for cmake build
    So, I modified cmakedefine to define

---
+++ kernel-open/common/inc/uvm_config.h
@@ -0,0 +1,14 @@
+#ifndef __UVM_CONFIG_H__
+#define __UVM_CONFIG_H__
+
+/*
+#cmakedefine UVM_MEM_COLORING
+#cmakedefine UVM_USER_MEM_COLORING
+#cmakedefine UVM_TEST_MEM_COLORING
+*/
+
+#define UVM_MEM_COLORING
+#define UVM_USER_MEM_COLORING
+#define UVM_TEST_MEM_COLORING
+
+#endif // __UVM_CONFIG_H__
